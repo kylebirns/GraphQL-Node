647dc9790af1f199b11153701266cc1cd762c3f7
kylebirns <kylebirns@gmail.com>
2019-07-11 12:30:06 -0400

    auth complete

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
 async function signup(parent, args, context, info) {
     // 1
     const password = await bcrypt.hash(args.password, 10)
@@ -34,6 +38,15 @@ async function signup(parent, args, context, info) {
       token,
       user,
     }
+  }
+
+  function post(parent, args, context, info) {
+    const userId = getUserId(context)
+    return context.prisma.createLink({
+      url: args.url,
+      description: args.description,
+      postedBy: { connect: { id: userId } },
+    })
   }
 
   module.exports = {
